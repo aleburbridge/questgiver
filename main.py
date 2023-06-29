7acd3e862de521afbb3f3f682e5d0899233d5015
aleburbridge <aleburbridge@gmail.com>
2023-06-29 13:35:28 -0700

    le chat has arrived

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template, request, session, redirect, url_for
-from flask_socketio import join_room, leave_room, send, SocketIO, emit
+from flask_socketio import join_room, leave_room, send, SocketIO
 import random
 from string import ascii_uppercase
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "hglhasglkhsdlgh"
-socketio = SocketIO(app)
+socketio = SocketIO(app, cors_allowed_origins="*")
 
+# -----Logic for rooms------
 rooms = {}
 
 def generate_room_code(length):
@@ -24,7 +25,7 @@ def create_room_from_code(code):
     rooms[code] = {"players": [], "host": ""}
     session["roomCode"] = code
 
-# -----Routes below this line ------
+# -----Routes------
 @app.route("/", methods=["POST", "GET"])
 def home():
     session.clear()
@@ -39,11 +40,11 @@ def home():
             create_room_from_code(roomCode)
             return render_template("home.html", roomCode=roomCode, codeInput=roomCode)
         
-        if join != False and not name:
-            return render_template("home.html", error="Please enter a name")
-        
-        if join != False and not codeInput:
-            return render_template("home.html", error="Please enter a room code")
+        if join != False:
+            if not name:
+                return render_template("home.html", error="Please enter a name")
+            if not codeInput:
+                return render_template("home.html", error="Please enter a room code")
        
         if codeInput not in rooms:
             return render_template("home.html", error="Room does not exist")
@@ -64,19 +65,27 @@ def home():
 
     return render_template("home.html")
 
-@app.route("/lobby")
+@app.route("/lobby", methods=["POST", "GET"])
 def lobby():
     roomCode = session.get("roomCode")
+    
+    if request.method == "POST":
+        return redirect(url_for("inGame"))
+
+
     return render_template(
         "lobby.html", 
         players = [player["name"] + " (you)" if player["name"] == session["name"] else player["name"] for player in rooms[roomCode]["players"] if player["inLobby"]],
         code = roomCode
     )
 
-@app.route("/game")
-def game():
-    return render_template("game.html")
-    # Assign Roles
+@app.route("/inGame")
+def inGame():
+    return render_template(
+        "inGame.html",
+        code = session.get("roomCode")
+    )
+    
 
 @socketio.on("connect")
 def connect(auth):
@@ -92,5 +101,13 @@ def connect(auth):
     send({"name": name}, to=roomCode)
     print(f"{name} joined room {roomCode}")
 
+# -----Chat------
+@socketio.on('message')
+def handle_message(message):
+    print("Received message: " + message)
+    if message != "User connected!":
+        send(message, broadcast=True)
+
+# -----Run------
 if __name__ == "__main__":
     socketio.run(app, debug=True)
